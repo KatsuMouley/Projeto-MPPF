9de26767bfd223686e7c8e72b0bd677ea082738d
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-11-05 07:24:02 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Projeto-MPPF/', // substitua pelo nome do seu repositório
+  // outras configurações
   plugins: [react()],
 })
