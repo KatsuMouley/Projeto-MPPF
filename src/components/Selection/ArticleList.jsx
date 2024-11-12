692623c48a00b9e6b215b40db4098df181e14e19
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-11-12 04:43:40 -0300

    better buttons for articles and questions

--- src/components/Selection/ArticleList.jsx
+++ src/components/Selection/ArticleList.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importando o Link
-import articleImage1 from '../../assets/Articles Images/Article 1/Thumb/image.png'; // Imagem do artigo
-import articleImage2 from '../../assets/Articles Images/Article 2/image13.png'; // Imagem do artigo
-import articleImage3 from '../../assets/Articles Images/Article 2/image12.png'; // Imagem do artigo
-import articleImage4 from '../../assets/Articles Images/Article 2/image4.png'; // Imagem do artigo
-import articleImage5 from '../../assets/Articles Images/Article 2/image16.png'; // Imagem do artigo
-
+import { Link } from 'react-router-dom';
+import articleImage1 from '../../assets/Articles Images/Article 1/Thumb/image.png';
+import articleImage2 from '../../assets/Articles Images/Article 2/image13.png';
+import articleImage3 from '../../assets/Articles Images/Article 2/image12.png';
+import articleImage4 from '../../assets/Articles Images/Article 2/image4.png';
+import articleImage5 from '../../assets/Articles Images/Article 2/image16.png';
 
 import Navbar from '../Navbar/Navbar';
 import About from '../Article/AboutSection';
@@ -15,7 +14,7 @@ const ArticleList = () => {
   const articles = [
     {
       title: 'A importância de se concientizar a respeito de crimes digitais.',
-      path: '/Projeto-MPPF/article/1', // Defina o caminho da rota correspondente
+      path: '/Projeto-MPPF/article/1',
     },
     {
       title: 'Top 10 crimes cibernéticos no qual você deve tomar cuidado',
@@ -33,11 +32,11 @@ const ArticleList = () => {
       title: 'Perguntas Frequentes',
       path: '/Projeto-MPPF/faq',
     }
-  ]; // Títulos e caminhos dos artigos
+  ];
 
   const articlesImages = [
     articleImage2,
-    articleImage1, // Adicione mais imagens aqui se necessário
+    articleImage1,
     articleImage3,
     articleImage5,
     articleImage4
@@ -48,13 +47,10 @@ const ArticleList = () => {
       <h1 className="text-2xl font-bold mb-4">Lista de Artigos disponíveis</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {articles.map((article, index) => (
-          <div key={index} className="border rounded-lg p-4 shadow-lg hover:cursor-pointer hover:scale-105 transition duration-300">
+          <Link to={article.path} key={index} className="border rounded-lg p-4 shadow-lg hover:cursor-pointer hover:scale-105 transition duration-300">
             <img className='w-full h-48 object-cover mb-4' src={articlesImages[index] || articleImage1} alt={`Imagem do ${article.title}`} />
-            <Link to={article.path} className="text-lg font-semibold">
-              {article.title}
-            </Link>
-            {/* Você pode adicionar mais detalhes sobre o artigo aqui */}
-          </div>
+            <div className="text-lg font-semibold">{article.title}</div>
+          </Link>
         ))}
       </div>
     </div>
--- src/components/Selection/Questionario.jsx
+++ src/components/Selection/Questionario.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importando o Link
 
 import Navbar from '../Navbar/Navbar';
 import About from '../Article/AboutSection';
 import Article from '../Article/ArticleSection';
 
-
-import articleImage1 from '../../assets/Questions Images/image1.png'; // Imagem do artigo
-import articleImage2 from '../../assets/Questions Images/image4.png'; // Imagem do artigo
-import articleImage3 from '../../assets/Questions Images/image3.png'; // Imagem do artigo
+import articleImage1 from '../../assets/Questions Images/image1.png';
+import articleImage2 from '../../assets/Questions Images/image4.png';
+import articleImage3 from '../../assets/Questions Images/image3.png';
 
 const Questionario = () => {
   const questionarios = [
     {
       title: 'Pesquisa a respeito de crimes cibernéticos',
-      url: 'https://forms.gle/Kki5qP7Cfv2tqvir7', // URL do questionário externo
+      url: 'https://forms.gle/Kki5qP7Cfv2tqvir7',
     },
     {
       title: 'Você já foi vítima de algum crime cibernético?',
@@ -24,10 +22,9 @@ const Questionario = () => {
       title: 'Questionário sobre Propostas de Soluções para Crimes Digitais',
       url: 'https://forms.gle/54ohmdP3DAVBLzi38',
     },
-  ]; // Títulos e URLs dos questionários
+  ];
 
   const questionariosImages = [
-    // Adicione imagens para os questionários se necessário
     articleImage1,
     articleImage2,
     articleImage3,
@@ -38,13 +35,17 @@ const Questionario = () => {
       <h1 className="text-2xl font-bold mb-4">Lista de Questionários disponíveis</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {questionarios.map((questionario, i) => (
-          <div key={i} className="border rounded-lg p-4 shadow-lg hover:cursor-pointer hover:scale-105 transition duration-300">
+          <a 
+            href={questionario.url} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            key={i} 
+            className="border rounded-lg p-4 shadow-lg hover:cursor-pointer hover:scale-105 transition duration-300"
+          >
             <img className='w-full h-48 object-cover mb-4' src={questionariosImages[i] || ''} alt={`Imagem do ${questionario.title}`} />
-            <a href={questionario.url} target="_blank" rel="noopener noreferrer" className="text-lg font-semibold">
-              {questionario.title}
-            </a>
+            <div className="text-lg font-semibold">{questionario.title}</div>
             {/* Você pode adicionar mais detalhes sobre o questionário aqui */}
-          </div>
+          </a>
         ))}
       </div>
     </div>
