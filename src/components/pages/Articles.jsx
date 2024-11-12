af9e51b2f6a5ea343567bd355f9cd91196125c7d
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-11-12 04:30:45 -0300

    .

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import About from '../Article/AboutSection';
-import ArticleGrid from '../Selection/ArticleList';
+import ArticleList from '../Selection/ArticleList';
 import Footer from '../Navbar/Footer';
 
 const Articles = () => {
   return (
   <div >
-      <ArticleGrid />
+      <ArticleList />
       <Footer/>
   </div>
   );
